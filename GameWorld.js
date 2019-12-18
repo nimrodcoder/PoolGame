6148de89bd6d89297a9100236daefdb86b06561f
niharika_dentalkart <niharika@dentalkart.com>
2019-12-19 03:06:56 +0530

    stick and ball move

---
+++ GameWorld.js
@@ -0,0 +1,20 @@
+const DELTA = 1/100;
+
+function GameWorld(){
+    this.whiteBall = new Ball(new Vector2(413,413));
+    this.stick = new Stick(new Vector2(413,413), this.whiteBall.shoot.bind(this.whiteBall));
+}
+
+GameWorld.prototype.update = function(){
+    
+    this.stick.update();
+    this.whiteBall.update(DELTA);
+}
+
+GameWorld.prototype.draw = function(){
+
+    Canvas.drawImage(sprites.background, {x:0, y:0});
+
+    this.stick.draw();
+    this.whiteBall.draw();
+} 
